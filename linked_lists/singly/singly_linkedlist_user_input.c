bc5dc7e3889db38ffca821a6d6dddb30f0442a80
AnuragShirodkar <anni14xx@gmail.com>
2026-05-04 02:04:29 +0530

    feat: add singly linked list with user input creation and display

---
+++ linked_lists/singly/singly_linkedlist_user_input.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+void display(struct node *head){
+
+    if(head == NULL){
+        printf("The list is empty\n");
+        return;
+    }
+
+    struct node *temp = head;
+
+    while(temp != NULL){
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+int main(){
+
+    int num;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &num);
+
+    struct node *head = NULL;
+    struct node *temp = NULL;
+
+    for(int i = 0; i < num; i++){
+
+        struct node *newNode = malloc(sizeof(struct node));
+
+        printf("Enter data for node %d: ", i+1);
+        scanf("%d", &newNode->data);
+
+        newNode->next = NULL;
+
+        if(head == NULL){
+            head = newNode;
+            temp = newNode;
+        }
+        else{
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    printf("\nLinked List:\n");
+    display(head);
+
+    return 0;
+}
