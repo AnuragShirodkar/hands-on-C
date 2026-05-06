69c54a4ab7f922007952374539ebfb19645255eb
AnuragShirodkar <anni14xx@gmail.com>
2026-05-06 23:38:19 +0530

    feat: add LCM calculation using loop

---
+++ numbers/numbers_lcm_using_loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,b,low, lcm;
+    printf("Enter 2 Numbers : ");
+    scanf("%d %d", &a,&b);
+
+    low=(a<b)?a:b;
+    while(1){
+        if(low%a==0 && low%b==0){
+            lcm = low;
+            break;
+        }
+        low++;
+    }
+    printf("The LCM of %d and %d is %d", a,b,lcm);
+}
